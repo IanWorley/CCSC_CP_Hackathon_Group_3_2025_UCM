d9905baf50a0fa45ed4a6e3cbcc0d3225fdc9874
Michael <michaelharlow1@outlook.com>
2025-02-22 12:25:46 -0600

    (feat) react router

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,6 +10,9 @@ export default {
         sm: "calc(var(--radius) - 4px)",
       },
       colors: {
+        ucm: {
+          "red": "#d21e32"
+        },
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         card: {
