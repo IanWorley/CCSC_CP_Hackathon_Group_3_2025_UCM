be742897da5cdf0a71c977185514ae41ca297365
Ian Worley <ian@ianworley.xyz>
2025-02-22 17:43:57 -0600

    feat: add register route

---
+++ frontend/src/routes/Register.tsx
@@ -0,0 +1,136 @@
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+function Register() {
+  const formSchema = z.object({
+    username: z.string().min(3, {
+      message: "Username must be at least 2 characters.",
+    }),
+    password: z.string().min(8, {
+      message: "Password must be at least 8 characters.",
+    }),
+    studentEmail: z.string().email({
+      message: "Invalid email address",
+    }),
+    studentId: z.string().regex(/^\d{8}\d?$/, {
+      message: "Student ID must be 8 digits",
+    }),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+      studentEmail: "",
+      studentId: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    const registerFn = async () => {
+      const res = await fetch("/adduser", {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (res.ok) {
+        console.log("Registration successful");
+      } else {
+        alert("Invalid credentials");
+      }
+    };
+    registerFn();
+    form.reset();
+  }
+
+  return (
+    <div className="mx-auto p-2 ">
+      <h1 className="text-lg text-center font-bold">Register</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className=" p-4 ">
+          <FormField
+            control={form.control}
+            name="studentEmail"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Student Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Student Email" {...field} />
+                </FormControl>
+                <FormDescription>This is your student email.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="studentId"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Student ID</FormLabel>
+                <FormControl>
+                  <Input placeholder="Student ID" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="Username" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="Password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button className="  flex mx-auto w-3/4 mt-5" type="submit">
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
+
+export default Register;
